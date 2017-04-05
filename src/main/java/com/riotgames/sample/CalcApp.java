5fb4228b4f96eca1cb926c4e3dedc4b69d782eb6
ParkChangHan <qkck@naver.com>
2017-04-05 14:18:25 +0900

    perfect

--- src/main/java/com/riotgames/sample/CalcApp.java
+++ src/main/java/com/riotgames/sample/CalcApp.java
@@ -7,12 +7,25 @@ import java.util.Stack;
  */
 public class CalcApp {
     public double calc(String[] tokens) {
-    	Stack st = new Stack();
+     	Stack st = new Stack();
     	Stack st1 = new Stack();
     	double sum = 0;
-    	boolean p = false;;
+    	boolean p = false;
     	for(int i = 0 ; i < tokens.length; i++){
     		st.push(tokens[i]);
+    		
+    		if(st.peek().equals("(")){
+    			st.pop();
+    			for(int j = i; j < tokens.length; j++){
+    				if(tokens[j].equals(")")){
+    					String[] temp_tokens = new String[j-i-1];
+    					System.arraycopy(tokens, i+1, temp_tokens, 0, j-i-1);
+    					st.push(String.valueOf(calc(temp_tokens)));
+    					i = j;
+    					break;
+    				}
+    			}
+    		}
     		if(st.peek().equals("x") || st.peek().equals("/")){
     			st1.push(st.pop());
     			p = true;
@@ -23,11 +36,15 @@ public class CalcApp {
     			p = false;
     		}
     	}
-    	for(int i = 0 ; i < st.size()-1; i++){
-    		st.push(priority((String)st.pop(),(String)st.pop(),(String)st.pop()));
+    	for(int i = 0 ; i < st.size();){
+    		st1.push(st.pop());
     	}
-    	return Double.parseDouble((String)st.pop());
+    	
+    	for(int i = 0 ; i < st1.size()-1;){
+    		st1.push(priority((String)st1.pop(),(String)st1.pop(),(String)st1.pop()));
+    	}
 
+    	return Double.parseDouble((String)st1.pop());  
     }
     public String priority(String first, String op, String second){
     	Operator operator = Operator.findOperator(op);
