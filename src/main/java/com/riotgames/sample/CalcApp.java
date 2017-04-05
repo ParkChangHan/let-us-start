76fb729c71595833f28bcb2769f1f054086550b4
ParkChangHan <qkck@naver.com>
2017-04-05 13:21:31 +0900

    priority x,/

--- src/main/java/com/riotgames/sample/CalcApp.java
+++ src/main/java/com/riotgames/sample/CalcApp.java
@@ -1,24 +1,39 @@
 package com.riotgames.sample;
 
 import java.util.Arrays;
-
+import java.util.Stack;
 /**
  * Calculator application
  */
 public class CalcApp {
     public double calc(String[] tokens) {
-        final double firstOperand;
-        final double secondOperand;
-        firstOperand = Double.parseDouble(tokens[0]);
-        if (tokens.length > 2) {
-            secondOperand = Double.parseDouble(tokens[2]);
-        } else {
-            secondOperand = Double.parseDouble(tokens[1]);
-        }
-        final Operator operator = Operator.findOperator(tokens[1]);
+    	Stack st = new Stack();
+    	Stack st1 = new Stack();
+    	double sum = 0;
+    	boolean p = false;;
+    	for(int i = 0 ; i < tokens.length; i++){
+    		st.push(tokens[i]);
+    		if(st.peek().equals("x") || st.peek().equals("/")){
+    			st1.push(st.pop());
+    			p = true;
+    			continue;
+    		}
+    		if(p){
+    			st.push(priority((String)st.pop(),(String)st1.pop(),(String)st.pop()));
+    			p = false;
+    		}
+    	}
+    	for(int i = 0 ; i < st.size()-1; i++){
+    		st.push(priority((String)st.pop(),(String)st.pop(),(String)st.pop()));
+    	}
+    	return Double.parseDouble((String)st.pop());
 
-        return operator.evaluate(firstOperand, secondOperand);
-
+    }
+    public String priority(String first, String op, String second){
+    	Operator operator = Operator.findOperator(op);
+    	double firstOperand = Double.parseDouble(first);
+    	double secondOperand = Double.parseDouble(second);
+    	return String.valueOf(operator.evaluate(firstOperand, secondOperand));
     }
 
     public static void main( String[] args ) {
